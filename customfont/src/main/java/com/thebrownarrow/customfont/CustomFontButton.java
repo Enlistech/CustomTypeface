43156bc7313c551a165cc70b66bd7ba999f9fd3c
Jaydip Umaretiya <jaydip.umaretiya@gmail.com>
2017-02-02 14:35:32 +0530

    Add custom view inside library

---
+++ customfont/src/main/java/com/thebrownarrow/customfont/CustomFontButton.java
@@ -0,0 +1,44 @@
+package com.thebrownarrow.customfont;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.support.v7.widget.AppCompatButton;
+import android.util.AttributeSet;
+import android.util.Log;
+
+public class CustomFontButton extends AppCompatButton {
+
+    public CustomFontButton(Context context) {
+        super(context);
+    }
+
+    public CustomFontButton(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+        applyCustomTypeface(context, attributeSet);
+    }
+
+    public CustomFontButton(Context context, AttributeSet attributeSet, int defStyle) {
+        super(context, attributeSet, defStyle);
+        applyCustomTypeface(context, attributeSet);
+    }
+
+    private void applyCustomTypeface(Context context, AttributeSet attributeSet) {
+        TypedArray typedArray = context.obtainStyledAttributes(attributeSet, R.styleable.CustomTypeface);
+        String customFont = typedArray.getString(R.styleable.CustomTypeface_custom_typeface);
+        applyCustomTypeface(context, customFont);
+        typedArray.recycle();
+    }
+
+    public boolean applyCustomTypeface(Context context, String asset) {
+        Typeface typeface;
+        try {
+            typeface = Typeface.createFromAsset(context.getAssets(), asset);
+        } catch (Exception e) {
+            Log.e("Custom Typeface", "Unable to load typeface: " + e.getMessage());
+            return false;
+        }
+        setTypeface(typeface);
+        return true;
+    }
+}
