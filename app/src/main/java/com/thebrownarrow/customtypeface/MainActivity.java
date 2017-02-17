2944bf661785b2d864d46847b77f82608026f4d5
Jaydip Umaretiya <jaydip.umaretiya@gmail.com>
2017-02-16 23:00:23 -0800

    Add Chronometer for custom font

--- app/src/main/java/com/thebrownarrow/customtypeface/MainActivity.java
+++ app/src/main/java/com/thebrownarrow/customtypeface/MainActivity.java
@@ -2,6 +2,7 @@ package com.thebrownarrow.customtypeface;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Chronometer;
 import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         RadioGroup radioGroup = (RadioGroup)findViewById(R.id.rg_gender);
+        Chronometer chronometer = (Chronometer)findViewById(R.id.chronometer);
+
         radioGroup.clearCheck();
+        chronometer.start();
     }
 }
